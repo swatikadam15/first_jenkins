f3aa15404e9cdcfa60b72fde2c85bb7d2ea89f01
Swati Kadam <46994107+swatikadam15@users.noreply.github.com>
2026-01-18 15:05:36 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java running inside Docker 🚀");
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello from Java running inside Docker 🚀");
+//     }
+// }
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Docker + Jenkins!";
     }
 }
-
